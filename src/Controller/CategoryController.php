4fd1780a761e01a95b0b2fb389b0d02b127077f5
Hikari Ylan <ylan.nicolas52600@gmail.com>
2024-11-10 21:43:19 +0100

    Adding Category controller

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoryController extends AbstractController
+{
+    #[Route('/endings_category', name: 'app_endings_category')]
+    public function index(): Response
+    {
+        return $this->render('category/endings.html.twig', ['category_name' => 'les fins du jeu']);
+    }
+}

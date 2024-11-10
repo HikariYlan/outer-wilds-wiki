5b426ac8ceaabffd003ccaa2ae90da9b6f0da23c
Hikari Ylan <ylan.nicolas52600@gmail.com>
2024-11-11 00:45:24 +0100

    Displays titles based of the category name

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -11,6 +11,17 @@ class CategoryController extends AbstractController
     #[Route('/{category_name}_category', name: 'app_category')]
     public function index(string $category_name): Response
     {
-        return $this->render('category/'.$category_name.'.html.twig', ['category_name' => $category_name]);
+        $titles = [
+            'endings' => 'fins du jeu',
+            'achievements' => 'succès du jeu',
+            'gameplay' => 'gameplay du jeu',
+            'technologies' => 'technologies du jeu',
+            'planets' => 'planètes du jeu',
+            'species' => 'races du jeu',
+            'characters' => 'personnages du jeu',
+            'musics' => 'musiques du jeu'
+        ];
+
+        return $this->render('category/'.$category_name.'.html.twig', ['category_name' => $titles[$category_name] ]);
     }
 }

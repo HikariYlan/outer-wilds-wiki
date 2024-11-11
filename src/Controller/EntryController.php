6356528b1b23d0c3ee9785b3e107927a163c2b37
Hikari Ylan <ylan.nicolas52600@gmail.com>
2024-11-11 12:46:21 +0100

    FINALLY able to display entry title (That was a pain)

--- src/Controller/EntryController.php
+++ src/Controller/EntryController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\EntriesList;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EntryController extends AbstractController
 {
-    #[Route('/{entry_name}_entry', name: 'app_entry')]
-    public function index(string $entry_name): Response
+    #[Route('/{category_name}_category/{entry_name}_entry', name: 'app_entry')]
+    public function index(string $category_name, string $entry_name): Response
     {
-        return $this->render('entry/index.html.twig', [
-            'controller_name' => 'EntryController',
-        ]);
+        return $this->render('entry/index.html.twig',
+            [
+                'entry_title' => EntriesList::getEntryFromList(EntriesList::getEntriesList($category_name), $entry_name)
+            ]
+        );
     }
 }

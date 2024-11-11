52601aee29a36ddfcc4a324f89e18902b84bd431
Hikari Ylan <ylan.nicolas52600@gmail.com>
2024-11-11 01:43:16 +0100

    Adding endings entries list & parameter for generic entries list

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -8,6 +8,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
 {
+    private array $endings = [
+        'break-time-and-space-ending' => 'Break Time And Space Ending',
+        'echoes-of-the-eye-ending' => 'Echoes Of The Eye Endings',
+        'eye-of-the-universe-ending' => 'Eye Of The Universe Ending',
+        'game-over-ending' => 'Game Over',
+        'isolation-ending' => 'Isolation Ending',
+        'self-ending' => 'Self Ending',
+    ];
+
     #[Route('/{category_name}_category', name: 'app_category')]
     public function index(string $category_name): Response
     {
@@ -22,6 +31,13 @@ class CategoryController extends AbstractController
             'musics' => 'musiques du jeu'
         ];
 
-        return $this->render('category/'.$category_name.'.html.twig', ['category_name' => $category_name, 'category_title' => $titles[$category_name] ]);
+        return $this->render(
+            'category/'.$category_name.'.html.twig',
+            [
+                'category_name' => $category_name,
+                'category_title' => $titles[$category_name],
+                'pages_list' => $this->$category_name
+            ]
+        );
     }
 }

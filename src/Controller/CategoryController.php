5feac9a3f549d8df89f41060600c2a1abf7a6432
Hikari/Ylan <thedoctor52600@gmail.com>
2025-07-16 23:39:39 +0200

    Re-adding everything (no hope this works first time)

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\EntriesList;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoryController extends AbstractController
+{
+
+    #[Route('/{category_name}_category', name: 'app_category')]
+    public function index(string $category_name): Response
+    {
+        $titles = [
+            'endings' => 'fins du jeu',
+            'achievements' => 'succès du jeu',
+            'gameplay' => 'gameplay du jeu',
+            'technologies' => 'technologies du jeu',
+            'planets' => 'planètes du jeu',
+            'species' => 'races du jeu',
+            'characters' => 'personnages du jeu',
+            'musics' => 'musiques du jeu'
+        ];
+
+        return $this->render(
+            'category/'.$category_name.'.html.twig',
+            [
+                'category_name' => $category_name,
+                'category_title' => $titles[$category_name],
+                'entries_list' => EntriesList::getEntriesList($category_name)
+            ]
+        );
+    }
+}
---
+++ src/Controller/EntryController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\EntriesList;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EntryController extends AbstractController
+{
+    #[Route('/{category_name}_category/{entry_name}_entry', name: 'app_entry')]
+    public function index(string $category_name, string $entry_name): Response
+    {
+        return $this->render('entry/' . $category_name . '/' . $entry_name . '.html.twig',
+            [
+                'entry_title' => EntriesList::getEntryFromList(EntriesList::getEntriesList($category_name), $entry_name)
+            ]
+        );
+    }
+}
